59d152743c926a9b5dd987b8ad75ddd36785d907
petrus226 <lluisxl@gmail.com>
2018-06-25 11:43:29 +0200

    Initial contribution

---
+++ Gemfile
@@ -0,0 +1,7 @@
+ruby '>=2.5.1'
+source 'https://rubygems.org'
+
+gem 'rspec', '~> 3.7.0'
+
+
+gem 'rack', '~> 2.0'
---
+++ src/sucre.rb
@@ -0,0 +1,17 @@
+require 'rubygems'
+require 'sinatra'
+
+get '/add_task' do
+  erb :form
+end
+
+post '/add_task' do
+  status 200
+  @@tasks = params[:task]
+  "You add a new task:  #{params[:task]}"
+end
+
+get '/tasks' do
+  status 200
+  "You have to do: #{@@tasks}"
+end
