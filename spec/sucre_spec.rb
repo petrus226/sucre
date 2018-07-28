346b40a5a21dffbd15cc66f2353b82929514d045
petrus226 <lluisxl@gmail.com>
2018-07-28 20:41:21 +0200

    Delete task

--- spec/sucre_spec.rb
+++ spec/sucre_spec.rb
@@ -23,11 +23,7 @@ describe 'The Sucre app' do
   end
 
   it 'add two task' do
-    my_hash = {:name => "Arnau", :tasks => Array.new{Hash.new}}
-    my_json = JSON.generate(my_hash)
-
-    post '/add_task', :json => my_json, :task_name => task_name_1, :task_date => time
-    post '/add_task', :json => last_response.body, :task_name => "Limpiar", :task_date => 100
+    post '/add_task_not_json', :task_name => "Limpiar", :task_date => 100
     expect(last_response).to be_ok
     expect((JSON.parse(last_response.body)["tasks"]).length).to eq(2)
   end
@@ -36,13 +32,21 @@ describe 'The Sucre app' do
     my_hash = {:name => "Arnau", :tasks => Array.new{Hash.new}}
     my_json = JSON.generate(my_hash)
 
-    post '/add_task', :json => my_json, :task_name => task_name_1, :task_date => time
-
+    post '/add_task_not_json', :task_name => task_name_1, :task_date => time
+    post '/add_task_not_json', :task_name => task_name_1, :task_date => time
     get '/tasks'
     expect(last_response).to be_ok
-    puts "Respuestas get"
-    puts ((JSON.parse(last_response.body))[0])["name"]
     expect(((JSON.parse(last_response.body))[0])["name"]).to eq("Comprar fruta")
+  end
+
+  it 'delete a task' do
+
+    get '/read'
+    expect(last_response).to be_ok
+    old_length = (JSON.parse(last_response.body)["tasks"]).length
+    post '/delete_task'
+    expect(last_response).to be_ok
+    expect((JSON.parse(last_response.body)["tasks"]).length).to eq(old_length-1)
   end
 
   def task_name_1
--- src/sucre.rb
+++ src/sucre.rb
@@ -2,7 +2,12 @@ require 'rubygems'
 require 'sinatra'
 require 'json'
 
-@@hash_receive
+# @@hash_receive = {:name => "Arnau", :tasks => Array.new{Hash.new}}
+
+get '/read' do
+  status 200
+  JSON.generate(@@hash_receive)
+end
 
 post '/add_task' do
   status 200
@@ -12,13 +17,22 @@ post '/add_task' do
   JSON.generate(@@hash_receive)
 end
 
+post '/add_task_not_json' do
+  status 200
+  data_update = {:name => params[:task_name], :date => params[:task_date]}
+  @@hash_receive["tasks"].push(data_update)
+  JSON.generate(@@hash_receive)
+end
+
 get '/tasks' do
   status 200
-
    task_list = @@hash_receive["tasks"]
-   puts "HASH"
-   puts task_list
-   puts "JSON"
-   puts JSON.generate(task_list)
    JSON.generate(task_list)
+end
+
+post '/delete_task' do
+  status 200
+  puts @@hash_receive
+  @@hash_receive["tasks"].pop
+  JSON.generate(@@hash_receive)
 end
